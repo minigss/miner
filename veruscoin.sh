7e7a8179f50a633917b40946001d040309e5ac32
minigss <93747240+minigss@users.noreply.github.com>
2021-11-05 03:30:49 +0300

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+#veruscoin
+sudo apt update
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.34a/lolMiner_v1.34a_Lin64.tar.gz
+tar -xf lolMiner_v1.34a_Lin64.tar.gz
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x0e09e6ca7e7fd2b56922dc53cc7ab50af2d02d98.deneme1 -p x --cpu 4
